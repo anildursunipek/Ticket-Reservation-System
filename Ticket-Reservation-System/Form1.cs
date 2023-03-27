3623ee656bf3862c41d24bf0141ad74de57dfdb1
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-03-27 13:38:46 +0300

    Create new winforms project

---
+++ Ticket-Reservation-System/Form1.cs
@@ -0,0 +1,15 @@
+namespace Ticket_Reservation_System
+{
+    public partial class frmLoginUI : Form
+    {
+        public frmLoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
