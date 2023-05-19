9e0b5e5d7e2e5ca0fe2b75dab7777300ab721078
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-05-19 22:48:14 +0300

    Add User model

---
+++ Ticket-Reservation-System/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Reservation_System.Models
+{
+    internal class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Surname { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Email { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public int UserName { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public string Password { get; set; }
+        [MaxLength(1)]
+        [Required]
+        public string Gender { get; set; }
+        [MaxLength(40)]
+        [Required]
+        public string SecurityNumber { get; set; }
+        [MaxLength(30)]
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+
+
+
+    }
+}
