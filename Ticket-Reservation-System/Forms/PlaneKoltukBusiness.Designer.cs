535e07f7bf81d019bc2b199be4623741e0999658
lokmane-blbd <lokmaneblbd01@gmail.com>
2023-05-21 17:29:45 +0300

    commit

--- Ticket-Reservation-System/Forms/PlaneKoltukBusiness.Designer.cs
+++ Ticket-Reservation-System/Forms/PlaneKoltukBusiness.Designer.cs
@@ -28,10 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PlaneKoltukBusiness";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlaneKoltukBusiness));
+            SuspendLayout();
+            // 
+            // PlaneKoltukBusiness
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(1332, 897);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "PlaneKoltukBusiness";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PlaneKoltukBusiness";
+            ResumeLayout(false);
         }
 
         #endregion
