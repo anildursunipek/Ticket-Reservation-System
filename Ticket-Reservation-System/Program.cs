69db87ba1cb4fddddab740509142e78ecd8afc4e
lokmane-blbd <lokmaneblbd01@gmail.com>
2023-05-25 14:34:28 +0300

    commit

--- Ticket-Reservation-System/Program.cs
+++ Ticket-Reservation-System/Program.cs
@@ -1,3 +1,5 @@
+using Ticket_Reservation_System.Forms;
+
 namespace Ticket_Reservation_System
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Ticket_Reservation_System
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmMenu());
+            Application.Run(new plane());
         }
     }
 }
