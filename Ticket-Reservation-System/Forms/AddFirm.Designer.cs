8c2b343fb14ac225793212613a8c72c5cdef65eb
lokmane-blbd <lokmaneblbd01@gmail.com>
2023-06-02 23:39:28 +0300

    Firm formu

--- Ticket-Reservation-System/Forms/AddFirm.Designer.cs
+++ Ticket-Reservation-System/Forms/AddFirm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ticket_Reservation_System.Forms
 {
-    partial class Model
+    partial class AddFirm
     {
         /// <summary>
         /// Required designer variable.
@@ -32,56 +32,21 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Model));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddFirm));
+            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            maskedTextBox1 = new MaskedTextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
             label8 = new Label();
             label7 = new Label();
             label6 = new Label();
-            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
-            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
-            comboBox1 = new ComboBox();
-            label3 = new Label();
-            textBox1 = new TextBox();
-            label1 = new Label();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Gadugi", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            label8.ForeColor = Color.DarkGray;
-            label8.Location = new Point(421, 151);
-            label8.Name = "label8";
-            label8.Size = new Size(192, 28);
-            label8.TabIndex = 24;
-            label8.Text = "__________________";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Gadugi", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            label7.ForeColor = Color.DarkGray;
-            label7.Location = new Point(370, 123);
-            label7.Name = "label7";
-            label7.Size = new Size(292, 28);
-            label7.TabIndex = 23;
-            label7.Text = "____________________________";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Gadugi", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.ForeColor = Color.DarkGray;
-            label6.Location = new Point(351, 66);
-            label6.Name = "label6";
-            label6.Size = new Size(346, 57);
-            label6.TabIndex = 22;
-            label6.Text = "Model Ekleme";
             // 
             // guna2Button2
             // 
@@ -95,11 +60,11 @@
             guna2Button2.FillColor = Color.WhiteSmoke;
             guna2Button2.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Regular, GraphicsUnit.Point);
             guna2Button2.ForeColor = Color.Black;
-            guna2Button2.Location = new Point(455, 808);
+            guna2Button2.Location = new Point(586, 790);
             guna2Button2.Name = "guna2Button2";
             guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges2;
             guna2Button2.Size = new Size(242, 77);
-            guna2Button2.TabIndex = 26;
+            guna2Button2.TabIndex = 24;
             guna2Button2.Text = "İptal Et";
             // 
             // guna2Button1
@@ -114,105 +79,136 @@
             guna2Button1.FillColor = Color.FromArgb(98, 102, 244);
             guna2Button1.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
             guna2Button1.ForeColor = Color.White;
-            guna2Button1.Location = new Point(176, 808);
+            guna2Button1.Location = new Point(307, 790);
             guna2Button1.Name = "guna2Button1";
             guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges4;
             guna2Button1.Size = new Size(242, 77);
-            guna2Button1.TabIndex = 25;
+            guna2Button1.TabIndex = 23;
             guna2Button1.Text = "Onayla";
             // 
-            // comboBox1
+            // maskedTextBox1
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(250, 575);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(242, 40);
-            comboBox1.TabIndex = 31;
+            maskedTextBox1.Font = new Font("Segoe UI", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            maskedTextBox1.Location = new Point(90, 467);
+            maskedTextBox1.Mask = "(500) 00 00 000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(245, 46);
+            maskedTextBox1.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(90, 292);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(441, 46);
+            textBox1.TabIndex = 18;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Gadugi", 10.875F, FontStyle.Bold, GraphicsUnit.Point);
             label3.ForeColor = Color.DimGray;
-            label3.Location = new Point(315, 526);
+            label3.Location = new Point(90, 591);
             label3.Name = "label3";
-            label3.Size = new Size(103, 35);
-            label3.TabIndex = 30;
-            label3.Text = "Marka";
+            label3.Size = new Size(188, 35);
+            label3.TabIndex = 15;
+            label3.Text = "Firma Adresi";
             // 
-            // textBox1
+            // label2
             // 
-            textBox1.Font = new Font("Segoe UI", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(158, 367);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(441, 46);
-            textBox1.TabIndex = 29;
+            label2.AutoSize = true;
+            label2.Font = new Font("Gadugi", 10.875F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.DimGray;
+            label2.Location = new Point(90, 401);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 35);
+            label2.TabIndex = 14;
+            label2.Text = "Telefon No:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Gadugi", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Gadugi", 10.875F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.DimGray;
-            label1.Location = new Point(283, 310);
+            label1.Location = new Point(90, 239);
             label1.Name = "label1";
-            label1.Size = new Size(184, 38);
-            label1.TabIndex = 28;
-            label1.Text = "Model İsmi";
+            label1.Size = new Size(158, 35);
+            label1.TabIndex = 13;
+            label1.Text = "Firma İsmi";
             // 
-            // pictureBox3
+            // textBox2
             // 
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(1050, 432);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(301, 189);
-            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 34;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(706, 432);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(352, 189);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 33;
-            pictureBox2.TabStop = false;
+            textBox2.Font = new Font("Segoe UI", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(90, 657);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(1161, 46);
+            textBox2.TabIndex = 25;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(901, 239);
+            pictureBox1.Location = new Point(535, 203);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(301, 207);
+            pictureBox1.Size = new Size(831, 448);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 32;
+            pictureBox1.TabIndex = 26;
             pictureBox1.TabStop = false;
             // 
-            // Model
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Gadugi", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label8.ForeColor = Color.DarkGray;
+            label8.Location = new Point(400, 115);
+            label8.Name = "label8";
+            label8.Size = new Size(192, 28);
+            label8.TabIndex = 29;
+            label8.Text = "__________________";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Gadugi", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label7.ForeColor = Color.DarkGray;
+            label7.Location = new Point(349, 87);
+            label7.Name = "label7";
+            label7.Size = new Size(292, 28);
+            label7.TabIndex = 28;
+            label7.Text = "____________________________";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Gadugi", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.DarkGray;
+            label6.Location = new Point(330, 30);
+            label6.Name = "label6";
+            label6.Size = new Size(329, 57);
+            label6.TabIndex = 27;
+            label6.Text = "Firma Ekleme";
+            // 
+            // AddFirm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1404, 1060);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
-            Controls.Add(comboBox1);
-            Controls.Add(label3);
-            Controls.Add(textBox1);
-            Controls.Add(label1);
-            Controls.Add(guna2Button2);
-            Controls.Add(guna2Button1);
+            ClientSize = new Size(1378, 990);
             Controls.Add(label8);
             Controls.Add(label7);
             Controls.Add(label6);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(guna2Button2);
+            Controls.Add(guna2Button1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "Model";
+            Name = "AddFirm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Model";
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            Text = "AddFirm";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -220,17 +216,17 @@
 
         #endregion
 
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private MaskedTextBox maskedTextBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
         private Label label8;
         private Label label7;
         private Label label6;
-        private Guna.UI2.WinForms.Guna2Button guna2Button2;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
-        private ComboBox comboBox1;
-        private Label label3;
-        private TextBox textBox1;
-        private Label label1;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox1;
     }
 }
--- Ticket-Reservation-System/Forms/VehicleAdd.Designer.cs
+++ Ticket-Reservation-System/Forms/VehicleAdd.Designer.cs
@@ -48,6 +48,9 @@
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -58,7 +61,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Gadugi", 10.125F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.DimGray;
-            label1.Location = new Point(89, 143);
+            label1.Location = new Point(89, 226);
             label1.Name = "label1";
             label1.Size = new Size(169, 32);
             label1.TabIndex = 0;
@@ -69,7 +72,7 @@
             label2.AutoSize = true;
             label2.Font = new Font("Gadugi", 10.125F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = Color.DimGray;
-            label2.Location = new Point(89, 312);
+            label2.Location = new Point(89, 395);
             label2.Name = "label2";
             label2.Size = new Size(179, 32);
             label2.TabIndex = 1;
@@ -80,7 +83,7 @@
             label3.AutoSize = true;
             label3.Font = new Font("Gadugi", 10.125F, FontStyle.Bold, GraphicsUnit.Point);
             label3.ForeColor = Color.DimGray;
-            label3.Location = new Point(89, 509);
+            label3.Location = new Point(89, 592);
             label3.Name = "label3";
             label3.Size = new Size(138, 32);
             label3.TabIndex = 2;
@@ -91,7 +94,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Gadugi", 10.125F, FontStyle.Bold, GraphicsUnit.Point);
             label4.ForeColor = Color.DimGray;
-            label4.Location = new Point(368, 509);
+            label4.Location = new Point(368, 592);
             label4.Name = "label4";
             label4.Size = new Size(96, 32);
             label4.TabIndex = 3;
@@ -102,7 +105,7 @@
             label5.AutoSize = true;
             label5.Font = new Font("Gadugi", 10.125F, FontStyle.Bold, GraphicsUnit.Point);
             label5.ForeColor = Color.DimGray;
-            label5.Location = new Point(635, 509);
+            label5.Location = new Point(635, 592);
             label5.Name = "label5";
             label5.Size = new Size(87, 32);
             label5.TabIndex = 4;
@@ -111,7 +114,7 @@
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(89, 196);
+            textBox1.Location = new Point(89, 279);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(441, 46);
             textBox1.TabIndex = 5;
@@ -129,7 +132,7 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(89, 555);
+            comboBox1.Location = new Point(89, 638);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(242, 40);
             comboBox1.TabIndex = 8;
@@ -137,7 +140,7 @@
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(368, 555);
+            comboBox2.Location = new Point(368, 638);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(242, 40);
             comboBox2.TabIndex = 9;
@@ -145,7 +148,7 @@
             // comboBox3
             // 
             comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(635, 555);
+            comboBox3.Location = new Point(635, 638);
             comboBox3.Name = "comboBox3";
             comboBox3.Size = new Size(242, 40);
             comboBox3.TabIndex = 10;
@@ -162,7 +165,7 @@
             guna2Button1.FillColor = Color.FromArgb(98, 102, 244);
             guna2Button1.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
             guna2Button1.ForeColor = Color.White;
-            guna2Button1.Location = new Point(89, 746);
+            guna2Button1.Location = new Point(255, 817);
             guna2Button1.Name = "guna2Button1";
             guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges2;
             guna2Button1.Size = new Size(242, 77);
@@ -181,7 +184,7 @@
             guna2Button2.FillColor = Color.WhiteSmoke;
             guna2Button2.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Regular, GraphicsUnit.Point);
             guna2Button2.ForeColor = Color.Black;
-            guna2Button2.Location = new Point(368, 746);
+            guna2Button2.Location = new Point(534, 817);
             guna2Button2.Name = "guna2Button2";
             guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges4;
             guna2Button2.Size = new Size(242, 77);
@@ -218,12 +221,48 @@
             pictureBox3.TabIndex = 15;
             pictureBox3.TabStop = false;
             // 
-            // Vehicle
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Gadugi", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label8.ForeColor = Color.DarkGray;
+            label8.Location = new Point(412, 97);
+            label8.Name = "label8";
+            label8.Size = new Size(192, 28);
+            label8.TabIndex = 27;
+            label8.Text = "__________________";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Gadugi", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label7.ForeColor = Color.DarkGray;
+            label7.Location = new Point(361, 69);
+            label7.Name = "label7";
+            label7.Size = new Size(292, 28);
+            label7.TabIndex = 26;
+            label7.Text = "____________________________";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Gadugi", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.DarkGray;
+            label6.Location = new Point(356, 12);
+            label6.Name = "label6";
+            label6.Size = new Size(303, 57);
+            label6.TabIndex = 25;
+            label6.Text = "Araç Ekleme";
+            // 
+            // VehicleAdd
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1404, 1060);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
@@ -240,7 +279,7 @@
             Controls.Add(label2);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "Vehicle";
+            Name = "VehicleAdd";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Vehicle";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -267,5 +306,8 @@
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
+        private Label label8;
+        private Label label7;
+        private Label label6;
     }
 }
